5744a2e9e5e15c2c76f8a32e9bd68a405c2b7c96
Tim Becker <tim@presseverykey.com>
2021-01-19 17:37:04 +0100

    panelized

---
+++ panelize.sh
@@ -0,0 +1,14 @@
+# require kikit https://github.com/yaqwsx/KiKit
+
+
+kikit panelize grid \
+	-s 3 \
+	-g 1 4 \
+	--railsTb 10 \
+	--railsLr 10 \
+	--tabwidth 4 \
+	--tabheight 4 \
+	--mousebites 0.25 0.5 -0.25 \
+	--tooling 5 5 4 \
+	openair2.kicad_pcb \
+	openair2-panelized.kicad_pcb
